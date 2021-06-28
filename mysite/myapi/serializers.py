69debbbd30e139707c28ad31ed1b9f8ec85706ff
tekastx <tomasz.kiedrowski@stxnext.pl>
2021-06-28 11:15:03 +0200

    initial commit

---
+++ mysite/myapi/serializers.py
@@ -0,0 +1,8 @@
+# serializers.py
+from rest_framework import serializers
+from .models import Hero
+
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hero
+        fields = ('name', 'alias')
